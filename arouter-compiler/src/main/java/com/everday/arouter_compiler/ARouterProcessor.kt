2ed9f48d99cbc1c786df97a3ebf3614b6085043e
wangtao <wangtaohandsome@gamil.com>
2020-12-03 01:04:56 +0800

    '安卓工程参数传递给java工程'

--- arouter-compiler/src/main/java/com/everday/arouter_compiler/ARouterProcessor.kt
+++ arouter-compiler/src/main/java/com/everday/arouter_compiler/ARouterProcessor.kt
@@ -1,8 +1,14 @@
 package com.everday.arouter_compiler
 
+import com.everday.arouter_annotation.Route
 import com.google.auto.service.AutoService
+import com.squareup.javapoet.JavaFile
+import com.squareup.javapoet.MethodSpec
+import com.squareup.javapoet.TypeSpec
+import java.io.IOException
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
+import javax.lang.model.element.Modifier
 import javax.lang.model.element.TypeElement
 import javax.lang.model.util.Elements
 import javax.lang.model.util.Types
@@ -16,35 +22,67 @@ import javax.tools.DiagnosticListener
  **/
 @AutoService(Processor::class) //启动服务
 @SupportedAnnotationTypes("com.everday.arouter_annotation.Route") //注解信息
-@SupportedSourceVersion(SourceVersion.RELEASE_7) //环境版本
-
+@SupportedSourceVersion(SourceVersion.RELEASE_8) //环境版本
 //接收android工程传过来的参数
-@SupportedOptions("everDay")
-class ARouterProcessor:AbstractProcessor() {
+@SupportedOptions("moduleName")
+class ARouterProcessor : AbstractProcessor() {
     //操作Element的工具类（类，函数，属性；都是Element）
-    var element:Elements? = null
+    var element: Elements? = null
+
     //type(类信息)的工具类，包含用于操作TYpeMirror的工具方法
-    var typeTool:Types? = null
+    var typeTool: Types? = null
+
     //日志打印信息
-    var mess:Messager? = null
+    var mess: Messager? = null
+
     //文件生成器，类、资源就是最终要生成的文件  是需要Filer来完成的
-    var filer:Filer? = null
+    var filer: Filer? = null
 
     override fun init(processingEnvironment: ProcessingEnvironment?) {
         super.init(processingEnvironment)
         element = processingEnvironment?.elementUtils
         mess = processingEnvironment?.messager
         filer = processingEnvironment?.filer
-        val androidParams = processingEnvironment?.options?.get("everDay")
+        val androidParams = processingEnvironment?.options?.get("moduleName")
 
         //注解处理器里面抛出异常使用Diagnostic.Kind.ERROR
-        mess?.printMessage(Diagnostic.Kind.NOTE,"android工程参数========>$androidParams")
+        mess?.printMessage(Diagnostic.Kind.WARNING, "$androidParams  ==>")
 
     }
 
     //编译干活
-    override fun process(annotations: MutableSet<out TypeElement>?, round: RoundEnvironment?): Boolean {
-        mess?.printMessage(Diagnostic.Kind.NOTE,"process=========>开始编译")
-        return true
+    override fun process(
+        annotations: MutableSet<out TypeElement>?,
+        round: RoundEnvironment?
+    ): Boolean {
+        if (annotations.isNullOrEmpty()) {
+//            mess?.printMessage(Diagnostic.Kind.NOTE, "process=========> run")?
+        }
+        val elements = round?.getElementsAnnotatedWith(Route::class.java)
+        elements?.forEach {
+            //方法
+            val methodSpec = MethodSpec.methodBuilder("main")
+                .addModifiers(Modifier.PUBLIC,Modifier.STATIC)
+                .returns(Unit::class.java)
+                .addParameter(Array<String>::class.java,"args")
+                .build()
+
+            //2.类
+            val testClass = TypeSpec.classBuilder("EverDay1")
+                .addMethod(methodSpec)
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .build()
+            //3.包
+            val javaFile = JavaFile.builder("com.everday1.apt", testClass).build()
+            try {
+                javaFile.writeTo(filer)
+            }catch (e:IOException){
+                e.printStackTrace()
+            }
+        }
+
+
+//        mess?.printMessage(Diagnostic.Kind.NOTE, "process=========>")
+        return false
     }
 }
--- login/src/main/java/com/everday/login/activity/LoginActivity.kt
+++ login/src/main/java/com/everday/login/activity/LoginActivity.kt
@@ -2,8 +2,9 @@ package com.everday.login.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.everday.arouter_annotation.Route
 import com.everday.login.R
-
+@Route(path = "LoginActivity")
 class LoginActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
