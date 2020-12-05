7209742c582ffffe1899da7c0c48578db49f9056
wangtao <wangtaohandsome@gamil.com>
2020-12-05 21:58:49 +0800

    '使用apt技术生成java类'

--- app/src/main/java/com/everday/customarouter/MainActivity.kt
+++ app/src/main/java/com/everday/customarouter/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.everday.customarouter
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.ever_day.apt.HelloAPT
 import com.everday.arouter_annotation.Route
 
 @Route(path = "MainActivity")
@@ -10,5 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val aptName = HelloAPT.getName("apt生成的方法")
+        findViewById<TextView>(R.id.tv).text = aptName
     }
 }
